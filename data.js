58e361627f71670a0d83f6a0a3588b926a760a8d
DigitalGenius-ui <miladmarketing282@gmail.com>
2022-06-23 21:18:26 +0800

    added

--- data.js
+++ data.js
@@ -3,10 +3,17 @@ export const data = [
         id : 1,
         name : "Samsung Galaxy A51/6GB/128GB",
         disc : `Display Size : 6.5 Inch full rectangle
-        Resolusi : 1080 x 2400 (FHD+), Super AMOLED
+        Resolution : 1080 x 2400 (FHD+), Super AMOLED
         Rear Camera : 48.0 MP + 12.0 MP + 5.0 MP + 5.0 MP
         Front Camera : 32.0 MP
-        Prosesor : Exynos 9611, Baterai : 4000 mAh`,
+        Processor : Exynos 9611, Battery : 4000 mAh`,
+        description : [
+            "Display Size : 6.5 Inch full rectangle",
+            "Resolution : 1080 x 2400 (FHD+), Super AMOLED",
+            "Rear Camera : 48.0 MP + 12.0 MP + 5.0 MP + 5.0 MP",
+            "Front Camera : 32.0 MP",
+            "Processor : Exynos 9611, Battery : 4000 mAh"
+        ],
         imageURL : "https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/MTA-5914970/samsung_samsung-galaxy-a51--6gb-128gb-_full06.jpg",
         stars : 4,
         price : 200,
@@ -25,6 +32,14 @@ export const data = [
         Data Cable
         25W Travel Adaptor
         Ejection Pin`,
+        description : [
+            "Chipset: Qualcomm SM7125 Snapdragon 720G",
+            "Back Camera: 64MP + 12MP + 5MP + 5MP",
+            "Front Camera: 32MP",
+            "Layer Size: 6.5 Inch FHD+ sA Infinity O 90Hz + On-sceen FP",
+            "Battery: 4500 mAh",
+            "Operation System: Android 11; One UI 3.1 Smartphone Samsung Galaxy A52Data Cable 25W Travel Adaptor Ejection Pin",
+        ],
         imageURL : "http://static.bmdstatic.com/pk/product/large/60541a1e1cbe3.jpg",
         stars : 5,
         price : 300,
@@ -33,10 +48,18 @@ export const data = [
     {
         id : 3,
         name : "Galaxy S21 | S21+ | S21 Ultra 5G",
-        disc : `1. Measured diagonally, Galaxy S21 5G’s screen size is 6.2" in the full rectangle and 6.1" with accounting for the rounded corners; Galaxy S21+ 5G's screen size is 6.7" in the full rectangle and 6.5" with accounting for the rounded corners; Galaxy S21 Ultra 5G’s screen size is 6.8" in the full rectangle and 6.6" with accounting for the rounded corners; actual viewable area is less due to the rounded corners and camera hole.
+        disc : `1. Measured diagonally, Galaxy S21 5G’s screen size is 6.2" in the full rectangle and 6.1" with accounting for the rounded corners; Galaxy S21+ 5G's screen size is 6.7" in the full rectangle and 6.5"
+         with accounting for the rounded corners; Galaxy S21 Ultra 5G’s screen size is 6.8" in the full rectangle and 6.6" with accounting for the rounded corners; actual viewable area is less due to the rounded corners and camera hole.
         2. The ratio in the parenthesis indicates the ratio of focal length of the applicable lens to that of the Wide lens.
         3. 100X Space Zoom includes 10x Optic Zoom and goes up to 100x Super Resolution Zoom. Super Resolution Zoom includes digital zoom, which may cause some image deterioration. Hybrid Optic Zoom combines high-resolution image sensor and lens.
         4. Typical value tested under third-party laboratory condition. Rated (minimum) capacity is 3880mAh for Galaxy S21 5G, 4660mAh for Galaxy S21+ 5G and 4855mAh for Galaxy S21 Ultra 5G.`,
+        description : [
+            `Measured diagonally, Galaxy S21 5G’s screen size is 6.2" in the full rectangle and 6.1" with accounting for the rounded corners; Galaxy S21+ 5G's screen size is 6.7" in the full rectangle and 6.5"
+            with accounting for the rounded corners; Galaxy S21 Ultra 5G’s screen size is 6.8" in the full rectangle and 6.6" with accounting for the rounded corners; actual viewable area is less due to the rounded corners and camera hole.`,
+            `The ratio in the parenthesis indicates the ratio of focal length of the applicable lens to that of the Wide lens.`,
+            `100X Space Zoom includes 10x Optic Zoom and goes up to 100x Super Resolution Zoom. Super Resolution Zoom includes digital zoom, which may cause some image deterioration. Hybrid Optic Zoom combines high-resolution image sensor and lens.`,
+            `Typical value tested under third-party laboratory condition. Rated (minimum) capacity is 3880mAh for Galaxy S21 5G, 4660mAh for Galaxy S21+ 5G and 4855mAh for Galaxy S21 Ultra 5G.`,
+        ],
         imageURL : "https://images.samsung.com/is/image/samsung/p6pim/id/galaxy-s21/gallery/id-galaxy-s21-5g-g991-371107-sm-g991bzvgxid-thumb-368338136",
         stars : 5,
         price : 1000,
--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 import { data } from "../data.js";
 const carts = document.querySelector(".carts");
+const shops = document.querySelector(".shops");
+const shopDetails = document.querySelector(".shop-details");
+const layer = document.querySelector(".layer");
 
-const startHandler = (item) => {
+// cart array 
+let cart = getStorage();
+cart.forEach(() => addToCart());
+
+// star symbols 
+const starHandler = (item) => {
     return Array(item.stars).fill().map((_, i) => {
         return `<span class="stars">⭐</span>`
     })
 }
-
 const showData = () => {
     const renderData = data.map((item) => {
         return `
@@ -15,11 +22,11 @@ const showData = () => {
         <div class="details">
             <h1>${item.name}</h1>
             <p class="price">$${item.price}</p>
-            ${(startHandler(item)).join("")}
+            ${(starHandler(item)).join("")}
         </div>
         <div class="icons">
-            <button class="basket"><i class="fa-solid fa-cart-shopping"></i></button>
-            <button class="watch"><i class="fa-solid fa-eye"></i></button>
+            <button class="basket" data-id="${item.id}"><i class="fa-solid fa-cart-shopping"></i></button>
+            <button class="watch" data-id="${item.id}"><i class="fa-solid fa-eye"></i></button>
         </div>
         </div>
         `
@@ -27,9 +34,132 @@ const showData = () => {
     carts.innerHTML = (renderData).join("");
 }
 
+// show single details
 
+function showDetails(){
+    const watchBtn = [...document.querySelectorAll(".watch")];
+    watchBtn.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            const getId = btn.dataset.id;
+            const matchId = data.find((item) => item.id === +getId);
+            details(matchId);
+            layer.classList.add("active");
+            shopDetails.classList.add("active");
+        })
+    });
+}
+
+function details(item){
+    let singleItem = [];
+    for (const index of item.description) {
+        singleItem += `
+            <ul class="description">
+                <li>${index}</li>
+            </ul>
+        `
+    }
+    const detail = `
+    <button class="close"><i class="fa-solid fa-square-xmark"></i></button>
+    <div class="contents">
+        <img src="${item.imageURL}" alt="${item.name}">
+        <h1>${item.name}</h1>
+        <h2 class="price">$${item.price}</h2>
+        <h1>Description:</h1>
+        ${singleItem}
+        ${starHandler(item).join(" ")}
+    </div>
+    `
+    shopDetails.innerHTML = detail;
+}
+
+// close detail page 
+shopDetails.addEventListener("click", (e) => {
+    if(e.target.classList.contains("fa-square-xmark")){
+        layer.classList.remove("active")
+        shopDetails.classList.remove("active")
+    }
+})
+
+// push carts to array. 
+function addItem(){
+    const allBtn = [...document.querySelectorAll(".basket")]
+    allBtn.forEach((btn) => {
+        const getId = btn.dataset.id;
+        let getCart = data.find((item) => item.id === +getId);
+        let matchCartId = cart.find((item) => item.id === +getId);
+        if(matchCartId){
+            btn.disabled = true;
+            btn.style.color = "rgba(255, 255, 255, 0.400)";
+        }
+        if(cart.length < 1){
+            shops.innerHTML = `<h1 class="text">Your shopping cart is Empty!!!</h1>`
+        }
+        btn.addEventListener("click", ({target}) => {
+            target.disabled = true;
+            target.style.color = "rgba(255, 255, 255, 0.400)";
+            cart.push(getCart);
+            addToCart();
+            setStorage(cart);
+        })
+    })
+}
+
+// add data to cart 
+function addToCart(){
+    shops.innerHTML = ""
+    const addCart = cart.map((item) => {
+        const {price, imageURL, name, disc, id} = item;
+        return  `
+        <div class="shop">
+        <div class="shop-detail">
+            <img src="${item.imageURL}" alt="${name}">
+            <div class="shop-texts">
+                <h1>${name}</h1>
+            <p>${disc.substring(0,300)}...</p>
+                <h3 class="shop-price">$${price}</h3>
+                <div class="shop-btns">
+                    <button class="increase"><i class="fa-solid fa-plus"></i></button>
+                    <span>0</span>
+                    <button class="decrease"><i class="fa-solid fa-minus"></i></button>
+                </div>
+                <button class="delete" data-id="${id}">Delete</button>
+            </div>
+        </div>
+        </div>
+        `
+    });
+    shops.innerHTML = (addCart).join(" ");
+}
+
+// delete handler 
+shops.addEventListener("click", ({target}) => {
+    if(target.classList.contains("delete")){
+        target.parentElement.parentElement.parentElement.remove();
+        removeStorage(target);
+    }
+});
+
+function removeStorage(target){
+    const deleteId = target.dataset.id;
+    cart = cart.filter((item) => item.id !== +deleteId);
+    setStorage(cart);
+}
+
+// localStorage 
+function setStorage(item){
+    const store = localStorage.setItem("shop", JSON.stringify(item));
+    return store;
+}
+
+function getStorage(){
+    const store = localStorage.getItem("shop");
+    if(store === null) return []
+    return JSON.parse(store);
+}
 
 // when browser is loaded 
 window.addEventListener("DOMContentLoaded", () => {
     showData();
+    addItem();
+    showDetails()
 })
